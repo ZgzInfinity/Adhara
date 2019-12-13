477e27452777ed18d1e4f9686328301f77465f67
vpec <741294@unizar.es>
2019-12-13 17:53:03 +0100

    random 1s & 0s with random probability

--- gen_test_cases.py
+++ gen_test_cases.py
@@ -19,13 +19,18 @@ for i in range(NUM_CASES):
     # Initialize matrix with ones
     t = numpy.ones((num_products, num_products))
     col_init = 0
+    bound = numpy.random.uniform()
     # Iterate rows
     for row in range(num_products):
         # Iterate cols (upper triangle matrix)
         col_init = col_init + 1
         for col in range(col_init, num_products):
+            if(numpy.random.uniform() < bound):
+                cell_value = 0
+            else:
+                cell_value = 1
             # Fill both symmetric components in matrix with random 1 or 0
-            t[row][col] = t[col][row] = random.randint(0, 1)
+            t[row][col] = t[col][row] = cell_value
     # Open file
     fw = open("testSet_" + str(i + 1),"w+")
     # Open number of products
