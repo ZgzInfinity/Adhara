52470c54b5506757bb39bc9a035aec2bf7b09945
vpec <741294@unizar.es>
2020-01-15 21:07:32 +0100

    skew algorithm fixed

--- Practica2/src/Skew.java
+++ Practica2/src/Skew.java
@@ -2,108 +2,55 @@ import java.util.Arrays;
 
 public class Skew{
 	
-	
-	static boolean leq(Integer a1, Integer a2,   Integer b1, Integer b2) {
-		return(a1 < b1 || (a1 == b1 && a2 <= b2));
-	}
-	
-	static boolean leq(Integer a1, Integer a2, Integer a3,   Integer b1, Integer b2, Integer b3) {
-		return(a1 < b1 || (a1 == b1 && leq(a2,a3, b2,b3)));
-	}
-	
-	static void radixPass(Integer[] a, Integer[] b, Integer[] r, Integer n, Integer K) {
-		Integer[] c = new Integer[K + 1];
-		for(Integer i=0; i<=K;i++) c[i] = 0;
-		for(Integer i=0; i<n;i++) c[r[a[i]]]++;
-		for(Integer i=0,sum=0; i<=K;i++) {
-			Integer t = c[i];  c[i] = sum;  sum += t;
-		}
-		for(Integer i=0; i<n; i++) b[c[r[a[i]]]++] = a[i];
-	}
-	
-	private final static void radixPass(int [] src, int [] dst, int [] v, int vi,
-	        final int n, final int K, int start, int [] cnt)
-	    {
-	        // check counter array's size.
-	        assert cnt.length >= K + 1;
-	        Arrays.fill(cnt, 0, K + 1, 0);
+	/**
+     * Lexicographic order for pairs.
+     */
+    private final static boolean leq(int a1, int a2, int b1, int b2)
+    {
+        return (a1 < b1 || (a1 == b1 && a2 <= b2));
+    }
 
-	        // count occurrences
-	        for (int i = 0; i < n; i++)
-	            cnt[v[start + vi + src[i]]]++;
+    /**
+     * Lexicographic order for triples.
+     */
+    private final static boolean leq(int a1, int a2, int a3, int b1, int b2, int b3)
+    {
+        return (a1 < b1 || (a1 == b1 && leq(a2, a3, b2, b3)));
+    }
 
-	        // exclusive prefix sums
-	        for (int i = 0, sum = 0; i <= K; i++)
-	        {
-	            final int t = cnt[i];
-	            cnt[i] = sum;
-	            sum += t;
-	        }
+    /**
+     * Stably sort indexes from src[0..n-1] to dst[0..n-1] with values in 0..K from v. A
+     * constant offset of <code>vi</code> is added to indexes from src.
+     */
+    private final static void radixPass(int [] src, int [] dst, int [] v, int vi,
+        final int n, final int K, int start, int [] cnt)
+    {
+        // check counter array's size.
+        assert cnt.length >= K + 1;
+        Arrays.fill(cnt, 0, K + 1, 0);
 
-	        // sort
-	        for (int i = 0; i < n; i++)
-	            dst[cnt[v[start + vi + src[i]]]++] = src[i];
-	    }
-	
-	static void suffixArray(Integer[] s, Integer[] SA, Integer n, Integer K) {
-		Integer n0=(n+2)/3, n1=(n+1)/3, n2=n/3, n02=n0+n2;
-		Integer[] s12  = new Integer[n02 + 3];  s12[n02]= s12[n02+1]= s12[n02+2]=0;
-		Integer[] SA12 = new Integer[n02 + 3]; SA12[n02]=SA12[n02+1]=SA12[n02+2]=0;
-		Integer[] s0   = new Integer[n0];
-		Integer[] SA0  = new Integer[n0];
-		
-		for (Integer i=0, j=0;  i < n+(n0-n1);  i++) if (i%3 != 0) s12[j++] = i;
-		
-		// radixPass(s12 , SA12, s+2, n02, K);
-		radixPass(s12 , SA12, Arrays.copyOfRange(s, 2, s.length + 3), n02, K);
-		// radixPass(SA12, s12 , s+1, n02, K);
-		radixPass(SA12, s12 , Arrays.copyOfRange(s, 1, s.length + 2), n02, K);
-		radixPass(s12 , SA12, s  , n02, K);
-		
-		Integer name = 0, c0 = -1, c1 = -1, c2 = -1;
-		for(Integer i=0; i<n02;  i++) {
-			if (s[SA12[i]] != c0 || s[SA12[i]+1] != c1 || s[SA12[i]+2] != c2) {
-				name++; c0 = s[SA12[i]]; c1 = s[SA12[i]+1]; c2 = s[SA12[i]+2];
-			}
-			if (SA12[i]%3==1) {
-				s12[SA12[i]/3] = name;
-			}
-			else {
-				s12[SA12[i]/3 + n0] = name;
-			}
-		}
-		if (name < n02) {
-			suffixArray(s12, SA12, n02, name);
-			for(Integer i=0; i<n02;  i++) s12[SA12[i]]=i+1;
-		}
-		else {
-			for(Integer i=0; i<n02;  i++) SA12[s12[i] - 1] = i;
-		}
-		for (Integer i=0, j=0; i < n02; i++) if (SA12[i] < n0) s0[j++] = 3*SA12[i];
-		radixPass(s0, SA0, s, n0, K);
-		System.out.println(n0);
-		for (Integer p=0,  t=n0-n1,  k=0;  k < n;  k++) {
-			// #define GetI() (SA12[t] < n0 ? SA12[t]*3+1:(SA12[t] - n0)*3+2)
-			Integer i = SA12[t] < n0 ? SA12[t]*3+1:(SA12[t] - n0)*3+2;
-			Integer j = SA0[p];
-			if (SA12[t] < n0 ? leq(s[i], s12[SA12[t] + n0], s[j], s12[j/3]) :
-				leq(s[i],s[i+1],s12[SA12[t]-n0+1], s[j],s[j+1],s12[j/3+n0])) {
-				SA[k] = i;  t++;
-				if (t == n02) {
-					for (k++;  p < n0;  p++, k++) SA[k] = SA0[p];
-				}
-				else {
-					SA[k] = j;  p++;
-					if (p == n0) {
-						for (k++;  t < n02;  t++, k++) SA[k] = SA12[t] < n0 ? SA12[t]*3+1:(SA12[t] - n0)*3+2;
-					}
-				}
-			}
-		}
-	}
-	
-	
-	static final int[] suffixArray(int [] s, int [] SA, int n, final int K, int start, int [] cnt)
+        // count occurrences
+        for (int i = 0; i < n; i++)
+            cnt[v[start + vi + src[i]]]++;
+
+        // exclusive prefix sums
+        for (int i = 0, sum = 0; i <= K; i++)
+        {
+            final int t = cnt[i];
+            cnt[i] = sum;
+            sum += t;
+        }
+
+        // sort
+        for (int i = 0; i < n; i++)
+            dst[cnt[v[start + vi + src[i]]]++] = src[i];
+    }
+
+    /**
+     * Find the suffix array SA of s[0..n-1] in {1..K}^n. require s[n] = s[n+1] = s[n+2] =
+     * 0, n >= 2.
+     */
+    static final int[] suffixArray(int [] s, int [] SA, int n, final int K, int start, int [] cnt)
     {
         final int n0 = (n + 2) / 3, n1 = (n + 1) / 3, n2 = n / 3, n02 = n0 + n2;
 
@@ -122,6 +69,7 @@ public class Skew{
             if ((i % 3) != 0) s12[j++] = i;
 
         // lsb radix sort the mod 1 and mod 2 triples
+        cnt = ensureSize(cnt, K + 1);
         radixPass(s12, SA12, s, +2, n02, K, start, cnt);
         radixPass(SA12, s12, s, +1, n02, K, start, cnt);
         radixPass(s12, SA12, s, +0, n02, K, start, cnt);
@@ -210,28 +158,47 @@ public class Skew{
 
         return cnt;
     }
-	
-	static final int max(int [] input, int start, int length)
-    {
-        assert length >= 1;
 
-        int max = input[start];
-        for (int i = length - 2, index = start + 1; i >= 0; i--, index++)
+    /**
+     * Ensure array is large enough or reallocate (no copying).
+     */
+    private static final int [] ensureSize(int [] tab, int length)
+    {
+        if (tab.length < length)
         {
-            final int v = input[index];
-            if (v > max)
-            {
-                max = v;
-            }
+            tab = null;
+            tab = new int [length];
         }
-
-        return max;
+        
+        return tab;
     }
-	
-	
-	public static Integer [] buildSuffixArray(int [] input, int start, int length)
+
+    /**
+     * {@inheritDoc}
+     * <p>
+     * Additional constraints enforced by Karkkainen-Sanders algorithm:
+     * <ul>
+     * <li>non-negative (>0) symbols in the input (because of radix sort)</li>,
+     * <li><code>input.length</code> &gt;= <code>start + length + 3</code> (to simplify
+     * border cases)</li>
+     * <li>length >= 2</li>
+     * </ul>
+     * <p>
+     * If the input contains zero or negative values, or has no extra trailing cells,
+     * adapters can be used in the following way:
+     * 
+     * <pre>
+     * return new {@link DensePositiveDecorator}(
+     *      new {@link ExtraTrailingCellsDecorator}(
+     *          new {@link Skew}(), 3));
+     * </pre>
+     * 
+     * @see ExtraTrailingCellsDecorator
+     * @see DensePositiveDecorator
+     */
+    public static int [] buildSuffixArray(int [] input, int start, int length)
     {
-        final int alphabetSize = max(input, start, length);
+        final int alphabetSize = 256;
         final int [] SA = new int [length + 3];
 
         // Preserve the tail of the input (destroyed when constructing the array).
@@ -239,15 +206,10 @@ public class Skew{
         System.arraycopy(input, start + length, tail, 0, 3);
         Arrays.fill(input, start + length, start + length + 3, 0);
 
-        suffixArray(input, SA, length, 256, start, new int [256 + 2]);
+        suffixArray(input, SA, length, alphabetSize, start, new int [alphabetSize + 2]);
 
         // Reconstruct the input's tail.
         System.arraycopy(tail, 0, input, start + length, 3);
-//        for(int num :  SA) {
-//        	System.out.println(((Integer)num).toString());
-//        }
-        //Arrays.copyOfRange(SA, 0, SA.length - 3);
-        Integer[] result = Arrays.stream(SA).boxed().toArray(Integer[]::new);
-        return result;
+        return SA;
     }
 }
--- Practica2/src/SuffixArray.java
+++ Practica2/src/SuffixArray.java
@@ -9,7 +9,6 @@
  */ 
 
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -36,92 +35,8 @@ public class SuffixArray {
 		for(int i = 0; i < n; i++) {
 			sa[i] = (int) text.charAt(i);
 		}
-//		for(int i = n; i < n + 3; i++) {
-//			sa[i] = 0;
-//		}
-//		System.out.println(n);
-//		Integer[] result = new Integer[n];
-//		Skew.suffixArray(sa, result, n, 256);
-//		this.suffixIndex = Arrays.asList(result);
-//		this.suffixIndex = Arrays.asList(Skew.buildSuffixArray(sa, 0, n));
-		
-		this.suffixIndex = new ArrayList<>();
-		buildSuffixArray();
-		
-		List<Integer> suffixIndexCopy = Arrays.asList(Skew.buildSuffixArray(sa, 0, n));
-		for(int j = 0; j < n; j++) {
-			System.out.println(suffixIndex.get(j).toString() + " " + suffixIndexCopy.get(j).toString());
-		}
-		//n = n + 3;
-
-		
+		this.suffixIndex = Arrays.asList(Arrays.stream(Arrays.copyOfRange(Skew.buildSuffixArray(sa, 0, n), 0, n)).boxed().toArray(Integer[]::new));	
 	}
-
-	
-	
-	/**
-	 * Fills the list of suffixes with the content of the file
-	 */
-	public void buildSuffixArray() 
-	{ 
-		Suffix[] su = new Suffix[n];
-		// Store each char in suffix array with its index
-		for (int i = 0; i < n; i++) { 
-			su[i] = new Suffix(i, text.charAt(i), 0); 
-		}
-		// Store rank of each char
-		for (int i = 0; i < n; i++) {
-			if(i + 1 < n) {
-				su[i].next = su[i + 1].rank;
-			}
-			else {
-				su[i].next = -1;
-			}
-		}	
-
-		// First sorting step (according the 1st 2 char of each suffix)
-		Arrays.sort(su); 
-
-		// Create a new array
-		int[] ind = new int[n]; 
-		
-		// Sort suffix arrays using first 4 chars
-		for (int length = 4; length < 2 * n; length <<= 1) { 
-			// Assigning rank and index values to first suffix 
-			int rank = 0, prev = su[0].rank; 
-			su[0].rank = rank; 
-			ind[su[0].index] = 0;
-			// Calculate new ranks for each suffix
-			for (int i = 1; i < n; i++){
-				// If first rank and next ranks are same as 
-				// that of previous suffix in array, 
-				// assign the same new rank to this suffix
-				if (su[i].rank == prev && su[i].next == su[i - 1].next) { 
-					prev = su[i].rank; 
-					su[i].rank = rank; 
-				} 
-				else { 
-					// Otherwise increment rank and assign 
-					prev = su[i].rank; 
-					su[i].rank = ++rank; 
-				} 
-				ind[su[i].index] = i; 
-			} 
-			// Assign next rank to every suffix 
-			for (int i = 0; i < n; i++) { 
-				int nextP = su[i].index + length / 2; 
-				su[i].next = nextP < n ? 
-				su[ind[nextP]].rank : -1; 
-			}
-			// Sort the suffixes according to first k characters 
-			Arrays.sort(su); 
-		}
-		// Store result in suffixIndex list
-		for (int i = 0; i < n; i++) {
-			suffixIndex.add(su[i].index);
-		}
-	}
-
 
 	/**
 	 * @return the text
