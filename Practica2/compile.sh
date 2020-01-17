51f0cdcaa4ad3beb22c075fc1ba027c0d49aef61
vpec <741294@unizar.es>
2020-01-17 11:37:11 +0100

    new compile.sh

--- Practica2/compile.sh
+++ Practica2/compile.sh
@@ -1,8 +1,14 @@
 # Creat directory for binary files if not exists
 [ -d bin ] || mkdir bin
 echo "Preparing bzip3 libraries"
-make clean --directory Huffman > /dev/null
+# make clean --directory=Huffman > /dev/null
+cd Huffman
+make clean > /dev/null
+cd ..
 # Compile c++ programm (Huffman)
-make --directory Huffman > /dev/null
+# make --directory=Huffman > /dev/null
+cd Huffman
+make > /dev/null
+cd ..
 # Compile java programm
 javac -d bin src/*.java
